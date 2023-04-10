5f411e9e42ed3ed40f72879be2647fccf145b8c1
Assel S <120194838+Assel2109@users.noreply.github.com>
2023-04-10 21:29:13 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+// interface
+export interface IState {
+  player: {
+    name: string
+    url: string
+    type: string
+    club?: string
+  }[]
+}
+
+function App() {
+  const [player, setPlayer] = useState<IState['player']>([
+
+    {
+      name: 'Lionel Messi',
+      url: 'https://images.ole.com.ar/2021/11/05/a9AEaulQr_340x340__1.jpg',
+      type: 'Striker',
+      club: 'PSG'
+    }
+  ])
+
+
+  return (
+    <div className="App">
+      <h1>Make your dream football team.</h1>
+      <List player={player}></List>
+      <AddToList player={player} setPlayer={setPlayer}></AddToList>
+    </div>
+  );
+}
+
+export default App;
